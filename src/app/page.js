6e943a461ce1fc6ad51b020756c8d2e3b3c2555b
Kenji Emura <kenjiemura@gmail.com>
2024-01-04 23:59:22 +0900

    Added styling and functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,30 @@
+"use client"; // This component renders only on the client side
+
+import { useState } from "react";
+import { DateTime } from "luxon";
+import Calendar from "react-calendar";
+
 export default function Home() {
-  return <h1>Calendar</h1>;
+  const now = DateTime.now();
+
+  const [selectedWeek, setSelectedWeek] = useState([
+    now.startOf("week").toISODate(),
+    now.endOf("week").toISODate(),
+  ]);
+
+  const parseRange = (date) => {
+    const selectedDate = DateTime.fromJSDate(date);
+    setSelectedWeek([
+      selectedDate.startOf("week").toISODate(),
+      selectedDate.endOf("week").toISODate(),
+    ]);
+  };
+
+  return (
+    <div>
+      <h1 style={{ marginBottom: "2rem" }}>Para Diana con cariño 💕</h1>
+
+      <Calendar onChange={parseRange} value={selectedWeek} locale="ja-JP" />
+    </div>
+  );
 }
